fcc116a1a9b4fc4c9990b4acdd2909204da7ca70
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-06 20:56:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   def index
-    # provide a list of authors to the view for the filter control
     @authors = Author.all
 
     # filter the @posts list based on user input
